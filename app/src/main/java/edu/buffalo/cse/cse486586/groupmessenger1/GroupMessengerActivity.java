2c20d8ff3993869e7445058eb6a87088b10fed36
mmaddu <SomeThing$1>
2018-02-19 20:48:42 -0500

    Removed extra Log Statements

--- app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger1/GroupMessengerActivity.java
+++ app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger1/GroupMessengerActivity.java
@@ -46,8 +46,6 @@ public class GroupMessengerActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_messenger);
 
-
-
         TelephonyManager tel = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
         String portStr = tel.getLine1Number().substring(tel.getLine1Number().length() - 4);
         final String myPort = String.valueOf((Integer.parseInt(portStr) * 2));
@@ -57,13 +55,14 @@ public class GroupMessengerActivity extends Activity {
             ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
 
             new ServerTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, serverSocket);
-            Log.d(TAG, "CaN create a ServerSocket");
+            Log.d(TAG, "Can create a ServerSocket");
+
         } catch (IOException e) {
-            Log.d(TAG,e.toString());
+
             Log.d(TAG, "Can't create a ServerSocket");
             return;
         }
-        Log.d(TAG,"Done");
+
         final EditText editText = (EditText) findViewById(R.id.edit_text);
 
         editText.setOnKeyListener(new View.OnKeyListener() {
@@ -82,28 +81,16 @@ public class GroupMessengerActivity extends Activity {
                     tv.append("\n");
                     Log.d(TAG,msg);
 
-                     /*
-                    * Registers OnPTestClickListener for "button1" in the layout, which is the "PTest" button.
-                    * OnPTestClickListener demonstrates how to access a ContentProvider.
-                    */
-
                      findViewById(R.id.button1).setOnClickListener(
                           new OnPTestClickListener(tv, getContentResolver()));
 
 
                     new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, msg, myPort);
-                    Log.d(TAG,"ClientTaskCalled");
                     return true;
                 }
                 return false;
             }
         });
-        /*
-         * TODO: Use the TextView to display your messages. Though there is no grading component
-         * on how you display the messages, if you implement it, it'll make your debugging easier.
-         */
-
-
 
 
         findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
@@ -116,7 +103,7 @@ public class GroupMessengerActivity extends Activity {
                 tv.setMovementMethod(new ScrollingMovementMethod());
                 tv.append("\t"+msg);
                 tv.append("\n");
-                Log.d(TAG,msg);
+
 
                 findViewById(R.id.button1).setOnClickListener(
                         new OnPTestClickListener(tv, getContentResolver()));
@@ -127,15 +114,6 @@ public class GroupMessengerActivity extends Activity {
             }
         });
 
-        
-
-       // findViewById(R.id.button4).setOnClickListener();
-        
-        /*
-         * TODO: You need to register and implement an OnClickListener for the "Send" button.
-         * In your implementation you need to get the message from the input box (EditText)
-         * and send it to other AVDs.
-         */
     }
 
     private class ServerTask extends AsyncTask<ServerSocket, String, Void> {
@@ -146,8 +124,6 @@ public class GroupMessengerActivity extends Activity {
             uriBuilder.authority(authority);
             uriBuilder.scheme(scheme);
             return uriBuilder.build();
-
-
         }
         private final Uri mUri = buildUri("content", "edu.buffalo.cse.cse486586.groupmessenger1.provider");
 
@@ -156,13 +132,10 @@ public class GroupMessengerActivity extends Activity {
             @Override
         protected Void doInBackground(ServerSocket... sockets) {
             ServerSocket serverSocket = sockets[0];
-            Log.d(TAG,"ServerTaskCalled");
-
 
             try {
                 while (true) {
                     Socket socket = serverSocket.accept();
-                    Log.d("Server", "Received");
 
                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                     String message = in.readLine();
@@ -193,9 +166,7 @@ public class GroupMessengerActivity extends Activity {
         }
 
         protected void onProgressUpdate(String...strings) {
-            /*
-             * The following code displays what is received in doInBackground().
-             */
+
             String strReceived = strings[0].trim();
             Log.d("StringReceived", strReceived);
 
@@ -237,7 +208,7 @@ public class GroupMessengerActivity extends Activity {
 
                     Socket socket = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}),
                             Integer.parseInt(remotePort[i]));
-                    Log.d(TAG, "ClientTask socket created");
+
                     String msgToSend = msgs[0];
                     PrintWriter out =
                             new PrintWriter(socket.getOutputStream(), true);
@@ -246,11 +217,8 @@ public class GroupMessengerActivity extends Activity {
                     out.close();
                     socket.close();
 
-                    Log.d(TAG, "ClientTask socket closed");
 
                 }
-
-
 
             } catch(UnknownHostException e){
                     Log.e(TAG, "ClientTask UnknownHostException");
